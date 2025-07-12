6bcb1bcba3fb286fde3516e5bcde828d75da2c7c
firane <dthaibinh03@gmail.com>
2025-07-12 11:13:22 +0700

    Update CSS variables and refine Toaster component
    - Updated CSS variables for light and dark themes, enhancing color consistency and adding new font and shadow properties.
    - Removed richColors prop from Toaster component for improved simplicity.

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <ThemeProvider defaultTheme="light" storageKey="chemist-theme">
         <App />
-        <Toaster position="top-center" richColors />
+        <Toaster position="top-center" />
       </ThemeProvider>
     </Provider>
   </StrictMode>
